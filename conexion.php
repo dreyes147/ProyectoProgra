a839e6cf0f00b040b3357d9b4054ded9e1785613
Emmax77 <emmanuelguzman96@gmail.com>
2017-08-05 13:54:02 -0600

    Commit Conexion
    Conexion php a localhost

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+    die("Fallo en la conexion: " . $conn->connect_error);
+} 
+echo "Conectado Satisfactoriamente";
+?>
