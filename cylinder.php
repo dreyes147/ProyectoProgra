973fb1dcefca60bb38033e11db4e8fefe7ac87b3
danielbale12 <danielbale1594@gmail.com>
2017-08-07 19:34:56 -0600

    Cylinder
    CSS del cilindro

---
+++ cylinder.php
@@ -0,0 +1,107 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+		<style type="text/css">
+
+		*{
+    font-family: Gotham, "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+.tank{
+    position:relative;
+     margin:50px;
+}
+
+.tank .middle{
+    width:120px;
+    height:70px;
+    background-color:#444;
+    position:relative;
+}
+
+.tank .middle2{
+    width:120px;
+    height:60px;
+    background-color:#7FFF00;
+    position:relative;
+}
+
+.tank .middle3{
+    width:120px;
+    height:60px;
+    background-color:#ff0000;
+    position:relative;
+}
+
+.tank .middle4{
+    width:120px;
+    height:60px;
+    background-color:#FFFF00;
+    position:relative;
+}
+
+.tank .middle5{
+    width:120px;
+    height:40px;
+    background-color:#0000FF;
+    position:relative;
+}
+
+
+
+.tank .top{
+    width: 120px;
+    height: 50px;
+    background-color:#666;
+    -moz-border-radius: 60px / 25px;
+    -webkit-border-radius: 60px / 25px;
+    border-radius: 60px / 25px;
+    position:absolute;
+    top:-25px;
+}
+
+
+
+
+.tank .bottom{
+    width: 120px;
+    height: 50px;
+    background-color:#0000FF;
+    -moz-border-radius: 60px / 25px;
+    -webkit-border-radius: 60px / 25px;
+    border-radius: 60px / 25px;
+    position:absolute;
+    top:265px;
+    box-shadow:0px 0px 10px rgba(0,0,0,0.75)
+}
+
+
+
+
+
+</style>
+
+</head>
+<body>
+
+
+<div class="tank">
+    <div class="bottom"></div>
+    <div class="middle">A</div>
+    <div class="middle2">B</div>
+    <div class="middle3">C</div>
+    <div class="middle4">D</div>
+    <div class="middle5">E</div>
+    <div class="top"></div>  
+    </br>
+</div>
+
+
+
+
+</body>
+</html>
